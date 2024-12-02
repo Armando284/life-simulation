70d3022b3a59d418d9aa9f6eda2af1232dee54d6
Armando <72764913+Armando284@users.noreply.github.com>
2024-12-02 18:22:03 -0500

    feat: initialize canvas

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 
 import { Sim } from "./sim.js"
 import { config } from "./config.js"
+import { $ } from "./utils.js"
+
+const canvas = $('canvas#game')
+
+canvas.width = config.canvas.width
+canvas.height = config.canvas.height
+
+const ctx = canvas.getContext('2d')
+
+ctx.fillStyle = getComputedStyle($(':root')).getPropertyValue('--color-primary')
+ctx.fillRect(50, 50, 20, 20)
