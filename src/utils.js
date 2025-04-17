f3447ec6c8a4b5e3cac8c28498341d32ca357444
Armando <72764913+Armando284@users.noreply.github.com>
2025-04-17 01:21:26 -0400

    feat: adds color control to utils module

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,19 @@ export const $ = (tag) => {
   }
   return document.querySelector(tag)
 }
+
+const HEX_VALUES = '0123456789abcdef'
+
+const getOneRandomHexValue = () => HEX_VALUES[Math.floor(Math.random() * HEX_VALUES.length)]
+
+export const randomColor = () => {
+  const color = Array.from({ length: 6 }, getOneRandomHexValue)
+  return `#${color.join('')}`
+}
+
+export const colorSmallChange = (color) => {
+  // #rrggbb
+  const validIndexesToChange = [2, 4, 6]
+  const indexToChange = Math.floor(Math.random() * validIndexesToChange.length)
+  return `${color.slice(0, indexToChange)}${getOneRandomHexValue()}${indexToChange < color.length && color.slice(indexToChange + 1)}`
+}
